763ad7dbb132181ac259579faaa7745089839c96
Narayanan S <narayanan.s@in.abb.com>
2020-10-29 18:43:21 +0530

    Completed : day 2 challenges

--- 03_Day_Setting_Up/03_setting_up_boilerplate/src/index.js
+++ 03_Day_Setting_Up/03_setting_up_boilerplate/src/index.js
@@ -1,116 +1,119 @@
 // index.js
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 // To get the root element from the HTML document
-import asabenehImage from './images/asabeneh.jpg'
-
-// to import the doSomeMath from the math.js with or without extension
-import doSomeMath from './math.js'
-
-// to import the other modules
-// since these modules were not exported as default we have to desctructure
-import { addTwo, multiply, subtract } from './math.js'
-
-import * as everything from './math.js'
-console.log(addTwo(5, 5))
-console.log(doSomeMath.addTwo(5, 5))
-console.log(everything)
-// JSX element, header
-
-
-// JSX element, header
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
-const author = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
-}
-const date = 'Oct 2, 2020'
-
-// JSX element, header
-const header = (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div>
-  </header>
-)
-
-const numOne = 3
-const numTwo = 2
-
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
-
-const yearBorn = 1820
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
-
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
-
-const user = (
-  <div>
-    <img src={asabenehImage} alt='asabeneh image' />
+import asabenehImage from "./images/asabeneh.jpg";
+import htmlLogo from "./images/html_logo.png";
+import cssLogo from "./images/css_logo.png";
+import reactLogo from "./images/react_logo.png";
+const boxStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "50px",
+};
+const greyBackground = {
+  ...boxStyle,
+  background: "#ddd",
+};
+const blueground = {
+  textAlign: "center",
+  background: "blue",
+  color: "#fff",
+};
+const renderImages = (
+  <div style={greyBackground}>
+    <img src={htmlLogo} style={{ width: "180px" }} />
+    <img src={cssLogo} style={{ width: "180px" }} />
+    <img src={reactLogo} style={{ width: "180px" }} />
   </div>
-)
-
-// JSX element, main
-const main = (
-  <main>
-    <div className='main-wrapper'>
-      <p>
-        Prerequisite to get started{' '}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
-      {user}
+);
+const formSubscription = (
+  <div style={blueground}>
+    <h1>Subscribe</h1>
+    <p>Sign Up with your email address to recive news and updates</p>
+    <div>
+      <input
+        type="text"
+        name="firstname"
+        placeholder ="First name"
+        style={{ height: "30px", margin: "10px" }}
+      />
+      <input
+        type="text"
+        name="lastname"
+        placeholder ="Last name"
+        style={{ height: "30px", margin: "10px" }}
+      />
+      <input
+        type="text"
+        name="email"
+        placeholder ="Email"
+        style={{ height: "30px", margin: "10px" }}
+      />
     </div>
-  </main>
-)
+    <button
+      style={{
+        height: "30px",
+        width: "120px",
+        background: "orange",
+        margin: "10px",
+      }}
+    >
+      Subscribe
+    </button>
+  </div>
+);
 
-const copyRight = 'Copyright 2020'
+const skillSet = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React",
+  "Angular",
+  "C#",
+  "Node",
+  "Azure",
+  "Github",
+  "Phython",
+  "AI",
+  "Data Science",
+];
 
-// JSX element, footer
-const footer = (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>{copyRight}</p>
+const cardSection = (
+  <div>
+    <img src={asabenehImage} style={{ width: "180px", borderRadius: "50%" }} />
+    <h2>Abhisk</h2>
+    <small>Senior Developer</small>
+    <h2>Skills</h2>
+    <div style={{ padding: "20px" }}>
+      {skillSet.map((item) => {
+       return( <span
+          style={{
+            padding: "10px",
+            background: "pink",
+            color: "#fff",
+            margin: "10px",
+          }}
+        >
+          {item}
+        </span>)
+      })}
     </div>
-  </footer>
-)
-
+    <span>Joined  on 29-October-2020 </span>
+  </div>
+);
 // JSX element, app
 const app = (
-  <div className='app'>
-    {header}
-    {main}
-    {footer}
+  <div className="app">
+    {renderImages}
+    <br></br>
+    {formSubscription}
+    <br></br>
+    {cardSection}
   </div>
-)
+);
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 // we render the JSX element using the ReactDOM package
-ReactDOM.render(app, rootElement)
+ReactDOM.render(app, rootElement);
