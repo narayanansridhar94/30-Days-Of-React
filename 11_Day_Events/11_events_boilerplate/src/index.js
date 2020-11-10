aa3c90d2ac37e63c89c0fd2dec58518863bfb136
Narayanan S <narayanan.s@in.abb.com>
2020-11-10 12:03:55 +0530

    completing 11th day challenge

--- 11_Day_Events/11_events_boilerplate/src/index.js
+++ 11_Day_Events/11_events_boilerplate/src/index.js
@@ -4,9 +4,8 @@ import ReactDOM from 'react-dom'
 
 class App extends Component {
   state = {
-    firstName: '',
-    message: '',
-    key: '',
+    xValue : 0,
+    yValue  : 0
   }
   handleClick = (e) => {
     // e gives an event object
@@ -17,67 +16,23 @@ class App extends Component {
   }
   // triggered whenever the mouse moves
   handleMouseMove = (e) => {
-    this.setState({ message: 'mouse is moving' })
-  }
-  // to get value when an input field changes a value
-  handleChange = (e) => {
-    this.setState({
-      firstName: e.target.value,
-      message: e.target.value,
-    })
-  }
-
-  // to get keyboard key code when an input field is pressed
-  // it works with input and textarea
-  handleKeyPress = (e) => {
-    this.setState({
-      message:
-        `${e.target.value} has been pressed and the keycode is` + e.charCode,
-    })
-  }
-  // Blurring happens when a mouse leave an input field
-  handleBlur = (e) => {
-    this.setState({ message: 'Input field has been blurred' })
-  }
-  // This event triggers during a text copy
-  handleCopy = (e) => {
-    this.setState({
-      message: 'Using 30 Days Of React for commercial purpose is not allowed',
-    })
-  }
+    // console.log(e.clientX,e.clientY)
+    this.setState({ xValue: e.clientX  });
+    this.setState({ yValue: e.clientY  });
+  } 
   render() {
     return (
-      <div>
-        <h1>Welcome to the World of Events</h1>
-
-        <button onClick={this.handleClick}>Click Me</button>
-        <button onMouseMove={this.handleMouseMove}>Move mouse on me</button>
-        <p onCopy={this.handleCopy}>
-          Check copy right permission by copying this text
-        </p>
-
-        <p>{this.state.message}</p>
-        <label htmlFor=''> Test for onKeyPress Event: </label>
-        <input type='text' onKeyPress={this.handleKeyPress} />
-        <br />
-
-        <label htmlFor=''> Test for onBlur Event: </label>
-        <input type='text' onBlur={this.handleBlur} />
-
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor='firstName'>First Name: </label>
-            <input
-              onChange={this.handleChange}
-              name='firstName'
-              value={this.state.value}
-            />
-          </div>
-
-          <div>
-            <input type='submit' value='Submit' />
-          </div>
-        </form>
+      <div style={{ height:'100vh',width:'100%' }} onMouseMove={this.handleMouseMove}>
+        
+        <button style={{
+           left : this.state.xValue + 'px',
+           top : this.state.yValue + 'px',
+           backgroundColor : 'blue',
+           padding :'20px',
+           position : 'absolute',
+           color:'#fff'
+        }}>30 Days of  React</button>
+          
       </div>
     )
   }
